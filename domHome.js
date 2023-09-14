0b063a601fb1bdf1bd8ee8c8c554be45b7a4cdaf
Maks3502 <107855225+Maks3502@users.noreply.github.com>
2023-09-14 17:34:24 +0300

    Add files via upload

---
+++ domHome.js
@@ -0,0 +1,37 @@
+// Завдання  1
+
+// const btnRef = document.querySelector("button");
+// console.log(btnRef);
+
+// btnRef.style.border = "none";
+
+// btnRef.style.color = "black";
+
+// btnRef.style.backgroundColor = "blue"
+
+// btnRef.textContent = "Натисніть"
+
+
+// завдання 2
+
+// const imgRef = document.querySelector("img");
+// console.log(imgRef)
+
+// imgRef.src = "img/photo_2021-11-09_01-49-46.jpg".jpg";
+// imgRef.alt = "mountains";
+
+
+// Завдання 3 
+
+// const aRef = document.querySelector("a");
+// console.log(aRef);
+
+// aRef.textContent = "посилання";
+// aRef.href = "./portfolio.html"
+
+// const imgRef = document.querySelector("img");
+
+// imgRef.alt = "Cat";
+
+// console.log(imgRef)
+
